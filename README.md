c846cad440f801b86ff82ada31f32a720c7fea52
mariagomestavares <140862481+mariagomestavares@users.noreply.github.com>
2024-09-27 22:24:34 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FLASHCARD
+# Pagina-web-desenvolvendo-uma-ferramenta-interativa-de-estudo
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'felicidade',
+    'o que te faz feliz ?',
+    ' meus amigos e familia !!!'
+)
+
+criaCartao(
+    'Informática',
+    'quem e o pai da programação ',
+    'Walter Clark '
+)
+
+criaCartao(
+    'Qual é a melhor turma do CERB',
+    'A que mais se dedica as aulas do Prof. Rafael?',
+    'Estou em dúvida'
+)
+
+criaCartao(
+    'Ganhando o Mundo',
+    'Quem participou do Programa Ganhado o mundo em 2024?',
+    'Eloisa Amorim dos Santos'
+)
